a87c420fedb9382a191852421679782e9cc435f3
Quennie Yuan Win <kurtcayagatash@gmail.com>
2025-10-26 19:27:21 +0800

    refactor: clean up App.js by removing unused components and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import './App.css';
 import TeeLuxeNavbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -11,33 +10,6 @@ import ProductList from './pages/ProductList';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-
-function Men() {
-  return (
-    <div>
-      <h1>Men</h1>
-      <p>Explore our collection for men.</p>
-    </div>
-  );
-}
-
-function Women() {
-  return (
-    <div>
-      <h1>Women</h1>
-      <p>Explore our collection for women.</p>
-    </div>
-  );
-}
-
-function Sale() {
-  return (
-    <div>
-      <h1>Sale</h1>
-      <p>Check out our latest discounts and deals.</p>
-    </div>
-  );
-}
 
 function AppContent() {
   const location = useLocation();
@@ -50,9 +22,6 @@ function AppContent() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/men" element={<Men />} />
-          <Route path="/women" element={<Women />} />
-          <Route path="/sale" element={<Sale />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/products" element={<ProductList />} />
